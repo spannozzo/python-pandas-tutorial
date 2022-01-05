7747f577dc75e2ea0f1d4481373e9b9536702622
Salvatore P.C <salvatore.pannozzocapodiferro@gmail.com>
2022-01-05 06:57:11 +0000

    ok

---
+++ .learn/exercises/04-Series/app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+ages = [23,45,7,34,6,63,36,78,54,34]
+
+ser = pd.Series(ages,name='ages')
+
+print(ser)
---
+++ .learn/exercises/04.1-Date_range/app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+range = pd.date_range(start='2021-05-01',end='2021-05-12')
+
+print(range)
